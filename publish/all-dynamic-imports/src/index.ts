2b1b3b34777ca4890d6f118a640964ca5e1436dc
ja0nz <mail@ja.nz>
2022-02-25 20:56:41 +0100

    chore: played along with api explorer

--- publish/all-dynamic-imports/src/index.ts
+++ publish/all-dynamic-imports/src/index.ts
@@ -1,4 +1,28 @@
+// Copyright (c) Example Company. All rights reserved. Licensed under the MIT license.
+
+/**
+ * A library for building widgets.
+ *
+ * @remarks
+ * The `all-documentation-lib` defines the {@link IWidget} interface and {@link Widget} class,
+ * which are used to build widgets.
+ *
+ * @packageDocumentation
+ */
+
+/**
+ * Interface implemented by all widgets.
+ * @public
+ */
+export interface IWidget {
+    /**
+     * Draws the widget on the screen.
+     */
+    render(): void;
+}
+
 /**
  * This is the answer to all
+ * @beta
  */
-export const answer = 42;
+export const answer = 44;
