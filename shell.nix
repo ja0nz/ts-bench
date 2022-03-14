191caffabcaf50250c3fdd4cc1d7d8d23bd9d0f1
ja0nz <mail@ja.nz>
2022-03-14 18:31:26 +0100

    chore: added lsp server for svelte

--- shell.nix
+++ shell.nix
@@ -9,6 +9,7 @@ pkgs.mkShell {
    nodePackages.vscode-html-languageserver-bin
    nodePackages.vscode-css-languageserver-bin
    nodePackages.vscode-json-languageserver
+   nodePackages.svelte-language-server
    nodePackages.prettier
    nodePackages.eslint
   ];
