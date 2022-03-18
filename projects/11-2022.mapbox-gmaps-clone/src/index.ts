efbd149164ef34d8a8837ceda5bafc2dd90e791b
ja0nz <mail@ja.nz>
2022-03-18 12:28:10 +0100

    feat: new project mapbox WIP

---
+++ projects/11-2022.mapbox-gmaps-clone/src/index.ts
@@ -0,0 +1,52 @@
+import { h1 } from "@thi.ng/hiccup-html";
+import { $compile } from "@thi.ng/rdom";
+import { tw } from "twind";
+import mapbox, { Map, NavigationControl} from "mapbox-gl";
+//import * as MapboxDirections from '@mapbox/mapbox-gl-directions';
+
+const MB_TOKEN = import.meta.env.VITE_MB_TOKEN ?? "";
+
+// https://docs.mapbox.com/mapbox-gl-js/api/
+
+// https://docs.mapbox.com/help/glossary/access-token/
+mapbox.accessToken = <string>MB_TOKEN;
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Navigator
+// https://developer.mozilla.org/en-US/docs/Web/API/Geolocation
+navigator.geolocation.getCurrentPosition(success, error, {
+  enableHighAccuracy: true,
+});
+function error(err: GeolocationPositionError) {
+  setupMap([-0.12, 51.50])
+}
+function success(p: GeolocationPosition) {
+  const c = p.coords;
+  setupMap([c.longitude, c.latitude])
+}
+
+function setupMap(center: [number, number]) {
+  // https://docs.mapbox.com/mapbox-gl-js/api/map/
+  const map = new Map({
+    container: "app",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center,
+    zoom: 15
+  });
+
+  // https://docs.mapbox.com/mapbox-gl-js/api/markers/
+  // https://docs.mapbox.com/mapbox-gl-js/api/markers/#navigationcontrol
+  const nav = new NavigationControl({
+    visualizePitch: true
+  });
+  map.addControl(nav, 'bottom-right');
+
+
+
+  // https://github.com/mapbox/mapbox-gl-directions
+  // https://github.com/mapbox/mapbox-gl-directions/blob/master/API.md
+  // const directions = new MapboxDirections({
+  //   accessToken: MB_TOKEN,
+  // });
+
+  // map.addControl(directions, 'top-left');
+}
---
+++ projects/11-2022.mapbox-gmaps-clone/test/index.ts
@@ -0,0 +1,5 @@
+import {group} from '@thi.ng/testament';
+// Import * as assert from "assert";
+// import { } from "../src";
+
+group('11-2022.mapbox-gmaps-clone', {});
