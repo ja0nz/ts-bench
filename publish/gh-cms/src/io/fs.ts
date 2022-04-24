2147e3b021556ad6f62e6854052ce420d8597d02
ja0nz <mail@ja.nz>
2022-04-24 12:00:01 +0200

    refactor(gh-cms): moved io

---
+++ publish/gh-cms/src/io/fs.ts
@@ -0,0 +1,28 @@
+import { opendir, readFile } from 'node:fs/promises';
+import { join, extname } from 'node:path';
+import { readFileSync } from 'node:fs';
+
+export const readJSON = (path: string) => JSON.parse(<any>readFileSync(path));
+
+async function* fswalk(dir: string): AsyncGenerator<string> {
+  for await (const d of await opendir(dir)) {
+    const entry = join(dir, d.name);
+    if (d.isDirectory()) yield* fswalk(entry);
+    else if (d.isFile()) yield entry;
+  }
+}
+
+// Local content files
+export async function getInFs(contentDir: string): Promise<string[]> {
+  const fxs: string[] = [];
+  for await (const f of fswalk(contentDir)) {
+    if (extname(f) === '.md') {
+      const file = await readFile(f);
+      const buffer = Buffer.from(file).toString('utf-8');
+      fxs.push(buffer);
+      // Wrap in empty issue
+      //fxs.push({ id: '', state: '', body: buffer });
+    }
+  }
+  return fxs;
+}
---
+++ publish/gh-cms/src/io/net.ts
@@ -0,0 +1,52 @@
+import type { RequestParameters } from '@octokit/graphql/dist-types/types';
+import { graphql } from '@octokit/graphql';
+import { request } from '@octokit/request';
+import { URL } from 'node:url';
+import { GH_TOKEN } from '../api.js';
+
+export const qlClient = (url: string) => {
+  const { pathname } = new URL(url);
+  const [owner, repo] = pathname.split('/').filter(Boolean);
+  return graphql.defaults({
+    headers: {
+      authorization: `token ${GH_TOKEN}`,
+      // https://docs.github.com/en/graphql/overview/schema-previews#labels-preview
+      // Mutation.createLabel
+      // Mutation.deleteLabel
+      // Mutation.updateLabel
+      accept: 'application/vnd.github.bane-preview+json',
+    },
+    variables: {
+      owner,
+      repo,
+    },
+  });
+};
+
+
+/**
+ * Query the GitHub API with REST (only needed with milestone modification)
+ * @param url A repostitory url related to the injected GH_TOKEN
+ */
+export const restClient =
+  (url: string) =>
+  /**
+   * Fn2; Use with spread paramenter
+   * ```typescript
+   * restClient(url)(...spread)
+   * ```
+   * @param request_ A **GET / POST** uri; see https://docs.github.com/en/rest/reference
+   * @param payload A payload object with request parameters
+   */
+  async (request_: string, payload: RequestParameters) => {
+    const { pathname } = new URL(url);
+    const [owner, repo] = pathname.split('/').filter(Boolean);
+    return request.defaults({
+      headers: {
+        authorization: `token ${GH_TOKEN}`,
+        accept: 'application/vnd.github.v3+json',
+      },
+      owner,
+      repo,
+    })(request_, payload);
+  };
